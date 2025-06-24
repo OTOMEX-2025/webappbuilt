13b6601580b96a36181bbbe462060214a7627a6b
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-06-25 00:16:00 +0200

    theme moved inside hook

--- src/app/client/chat/page.js
+++ src/app/client/chat/page.js
@@ -1,20 +1,19 @@
 import dynamic from 'next/dynamic';
 import Loader from '@/components/Loader';
 import { useTheme } from '@/context/ThemeContext';
-// This component will only be rendered on the client side  
-const { theme } = useTheme();
-const ClientSideChatComponent = dynamic(
-
-
-
-  () => import('./ChatContent.client'),
-  { 
-    ssr: false,
-    loading: () => <Loader theme={theme}/> // Optional loading component
-  }
-);
 
 function Page() {
+  const { theme } = useTheme();
+  
+  // This component will only be rendered on the client side  
+  const ClientSideChatComponent = dynamic(
+    () => import('./ChatContent.client'),
+    { 
+      ssr: false,
+      loading: () => <Loader theme={theme}/> // Optional loading component
+    }
+  );
+
   return (
     <div>
       <h1>My Page</h1>
